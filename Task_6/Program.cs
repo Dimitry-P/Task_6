9f4e5c688c8c48a7b80bb598050758fc9465aad1
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:27:24 +0300

    Add project files.

---
+++ Task_6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class Program
+    {
+        //•	Задача 6: Напишите программу, которая
+        //находит наибольшее из N чисел, введенных пользователем.
+        public static string array;
+        public static int result = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите несколько чисел через запяту");
+            array = Console.ReadLine();
+            string[] pieces = array.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for(int i = 0; i<pieces.Length; i++)
+            {
+                if(result < int.Parse(pieces[i])){
+                    result = int.Parse(pieces[i]);
+                }
+            }
+            Console.WriteLine("Наибольшее число: " + result);
+        }
+    }
+}
